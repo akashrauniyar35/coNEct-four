535c56942b1fab308fdc8c7edb549a4057063730
Akash Rouniyar <akash@192-168-1-113.tpgi.com.au>
2019-08-11 20:23:31 +1000

    InitialSetup

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -11,6 +11,7 @@ const getCells = () => {
     const cells = [];
 
     for (let x = 0; x < CELLS_X; x++) {
+      reference_cell[y][x] = useRef(`${y}-${x}`);
       cells.push(<div className="Cell" key={`${y}-${x}`} />);
     }
 
